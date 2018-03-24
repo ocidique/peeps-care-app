bc6194514049bf10e8dabdc57a4886901bda43b0
Juho Pekkala <juho.pekkala@gmail.com>
2018-03-24 15:33:03 +0200

    Add firebase registration activity

--- app/src/main/java/com/care/peeps/MainActivity.java
+++ app/src/main/java/com/care/peeps/MainActivity.java
@@ -4,8 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+}
